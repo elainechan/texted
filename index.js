ac4a59f81edd68335eca7b640bc083969d1117cd
elainechan <elaine.chan@outlook.com>
2018-01-09 13:15:04 -0500

    Create and spin up Express server.

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const ngrok = require('ngrok'); // For testing the webhook
+
+const app = express() // Creates Express HTTP server
 
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 // Process application/json
 app.use(bodyParser.json())
 
 // Index route
-app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
-})
+app.get('/', (req, res) => {
+	res.send('Hello world, I am a chat bot.')
+});
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
+// Facebook verification
+app.get('/webhook/', (req, res) => {
 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
+		res.send(req.query['hub.challenge']);
 	}
-	res.send('Error, wrong token')
-})
+	res.send('Error, wrong token.');
+});
 
 // Spin up the server
-app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
-})
+app.listen(app.get('port'), () => {
+	console.log('Running on port', app.get(port));
+});
 
-app.post('/webhook/', function (req, res) {
+/*
+// Add API endpoint
+app.post('/webhook/', (req, res) => {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i]
@@ -41,7 +46,29 @@ app.post('/webhook/', function (req, res) {
 		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 	    }
     }
-    res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
+*/
+
+/*
+// added from APIAI
+var apiai = require('apiai');
+ 
+var app = apiai("<your client access token>");
+ 
+var request = app.textRequest('<Your text query>', {
+    sessionId: '<unique session id>'
+});
+ 
+request.on('response', function(response) {
+    console.log(response);
+});
+ 
+request.on('error', function(error) {
+    console.log(error);
+});
+ 
+request.end();
 
 const token = "<PAGE_ACCESS_TOKEN>"
+*/
