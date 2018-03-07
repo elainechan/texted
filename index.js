d60f79c5de412645a84d1c4ff7c4b827a749708f
elainechan <elaine.chan@outlook.com>
2018-03-07 07:49:24 -0500

    Removes key.

--- index.js
+++ index.js
@@ -41,4 +41,4 @@ app.post('/webhook/', (req, res) => {
     res.sendStatus(200);
 });
 //const token = process.env.FB_PAGE_ACCESS_TOKEN
-const token = 'AAMoJ7gecDIBABaW6Ix6AuCbg1MizErOtEffW7Lqin7ENbGzoxnpZBmx6LiVVru0cATKxa0BlSJxOZCZAvIglRXbQlvmbMcIUUZAkgAh0qufdBUda2l2FhoHD5ie3sv0w8wzrJ1q3L1V2UgfGLGJR7FR5JxetMZB0ZBkCesZCD6SgZDZD';
+const token = '';
